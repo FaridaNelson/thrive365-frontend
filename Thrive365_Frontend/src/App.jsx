7aaa6c124f2fc22b61e72079d5fadaa99c4513bf
SeroujK <seroujguidanian@gmail.com>
2026-01-18 09:04:40 -0800

    WIP: My local changes before pulling main

--- Thrive365_Frontend/src/App.jsx
+++ Thrive365_Frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import DashBoard from './components/DashBoard/DashBoard';
-import './App.css'
-import HeroSection from './components/HeroSection/HeroSection';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import DashBoard from "./components/DashBoard/DashBoard";
+import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
 
 
   return (
     <>
-    <Header/>
-    <HeroSection/>
-    {/* <DashBoard/> */}
-    <Footer/>
+      <Header/>
+      {/* <HeroSection /> */}
+      <DashBoard/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Thrive365_Frontend/src/components/Header/Header.jsx
+++ Thrive365_Frontend/src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import reactLogo from "../../assets/react.svg"
 import "./Header.css"
 
-function Header(){
+function Header(isLoggedIn){
 
     return(
         <div className='header'>
         <img src={reactLogo} alt="Thrive 365 Logo" />
         <div className="header__menu">
             <nav className="header__menu_nav">
-            <ul className="header__menu_nav_buttons">
+          { isLoggedIn? (<ul className="header__menu_nav_buttons">
                 <li>Home</li>
                 <li>About us</li>
                 <li>How to Use</li>
-            </ul>
+            </ul>) : null}
             </nav>
         </div>
        </div>
